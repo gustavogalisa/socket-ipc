7734406853e9f4a0b7123ea004ea3321ce04e113
Gustavo Galisa <gustavo.galisa@academico.ifpb.edu.br>
2021-11-06 15:24:01 -0300

    foi

--- src/com/gugawag/so/ipc/NomeClient.java
+++ src/com/gugawag/so/ipc/NomeClient.java
@@ -28,7 +28,7 @@ public class NomeClient {
 
 			PrintWriter pout = new PrintWriter(servidorSock.getOutputStream(), true);
 			// TODO Altere abaixo para enviar seu nome ao servidor
-			pout.println("SEU NOME AQUI");
+			pout.println("Gustavo Galisa");
 			servidorSock.close();
 		}
 		catch (IOException ioe) {
